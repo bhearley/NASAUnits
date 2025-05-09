665d7bc597e0a0bddd8472e64c72ec0085add300
bhearley <109361610+bhearley@users.noreply.github.com>
2025-05-09 12:58:50 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mymodule',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    description='A Python module with numpy and pandas functions.',
+    author='Your Name',
+    author_email='you@example.com',
+    url='https://github.com/yourusername/myproject',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
