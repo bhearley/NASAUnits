8e1936dcaa8c40629be3793aa4d87d43feec138a
bhearley <109361610+bhearley@users.noreply.github.com>
2025-05-09 13:10:59 -0400

    Create core.py

---
+++ nasaunits/core.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+def add_numbers(a, b):
+    return np.add(a, b)
+
+def create_dataframe(data):
+    """Create a pandas DataFrame from a dictionary."""
+    return pd.DataFrame(data)
