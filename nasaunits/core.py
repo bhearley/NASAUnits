38e59df76f2b0cbbe54ce204b0604c19d0fa96bc
bhearley <109361610+bhearley@users.noreply.github.com>
2025-05-09 14:04:18 -0400

    Update core.py

--- nasaunits/core.py
+++ nasaunits/core.py
@@ -1,29 +1,41 @@
+# UnitConversion is a function for converting units consistent with the NASA GRC Granta MI Database
+#
+# INPUTS:
+#    source_unit    the units to convert from
+#    source_value   the value for conversion
+#    target_unit    the units to convert to
+#
+# OUTPUTS:
+#    target_value   the value after conversion
+#    out_message    the output message
+
 def UnitConversion(source_unit, source_value, target_unit):
-    
     # Import Modules
     import requests
     import json
 
-    # Load the library
+    # Load the units library
     url = 'https://raw.githubusercontent.com/bhearley/NASAUnits/refs/heads/main/unit_library.json'
     response = requests.get(url)
-    
+
+    # Check for valid file
     if response.status_code == 200:
         U = response.json()  # Parse the JSON response
     else:
+        raise Exception("Error reading the material library.")
         U = None
-        
-    if U is not None:
 
-        # Convert to Global Variable
-        data = U[source_unit]
-        val = eval(data[0].replace('x', str(source_value)))
+    # Check for valid keys
+    if source_unit not in U.keys():
+        raise ValueError("Source Unit " + source_unit + " not found in material library.")
+    if target_unit not in U.keys():
+        raise ValueError("Target Unit " + target_unit + " not found in material library.")
         
-        data = U[target_unit]
-        target_val = eval(data[1].replace('x', str(val)))
+    # Convert to Global Variable
+    data = U[source_unit]
+    val = eval(data[0].replace('x', str(source_value)))
     
-        return target_val
+    data = U[target_unit]
+    target_value = eval(data[1].replace('x', str(val)))
 
-    else:
-        print('Error opening the unit library.')
-        return None
+    return target_value
